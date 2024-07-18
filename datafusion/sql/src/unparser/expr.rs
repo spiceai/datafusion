8f42022183460a2d8d31f3b152932ebeceac47f1
sgrebnov <sergei.grebnov@gmail.com>
2024-07-18 12:37:35 -0700

    Update

--- datafusion/sql/src/unparser/expr.rs
+++ datafusion/sql/src/unparser/expr.rs
@@ -953,7 +953,8 @@ impl Unparser<'_> {
 
     /// MySQL requires INTERVAL sql to be in the format: INTERVAL 1 YEAR + INTERVAL 1 MONTH + INTERVAL 1 DAY etc
     /// https://dev.mysql.com/doc/refman/8.4/en/expressions.html#temporal-intervals
-    /// Interval sequence can't be wrapped in (INTERVAL 1 YEAR + INTERVAL 1 MONTH ...) so we need to generate a single INTERVAL expression
+    /// Interval sequence can't be wrapped in brackets - (INTERVAL 1 YEAR + INTERVAL 1 MONTH ...) so we need to generate
+    /// a single INTERVAL expression so it works correct for interval substraction cases
     /// MySQL supports the DAY_MICROSECOND unit type (format is DAYS HOURS:MINUTES:SECONDS.MICROSECONDS), but it is not supported by sqlparser
     /// so we calculate the best single interval to represent the provided duration
     fn interval_to_mysql_expr(
@@ -1187,7 +1188,7 @@ impl Unparser<'_> {
                     );
                 }
                 ScalarValue::IntervalMonthDayNano(Some(v)) => {
-                    if v.nanoseconds % 1_000 > 0 {
+                    if v.nanoseconds % 1_000 != 0 {
                         return not_impl_err!(
                             "Unsupported IntervalMonthDayNano scalar with nanoseconds precision for IntervalStyle::MySQL"
                         );
