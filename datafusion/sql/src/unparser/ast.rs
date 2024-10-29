da721502142045e84a0de57df33bdc6d09b269d4
peasee <98815791+peasee@users.noreply.github.com>
2024-10-29 15:58:13 +1000

    fix: Ensure only tables or aliases that exist are projected (#52) fix: More dangling references (#54)
    UPSTREAM NOTE: This PR was attempted to be upstreamed in https://github.com/apache/datafusion/pull/13405 - but it was not accepted due to the complexity it brought. Phillip needs to figure out what a good solution that solves our problem and can be upstreamed is.

--- datafusion/sql/src/unparser/ast.rs
+++ datafusion/sql/src/unparser/ast.rs
@@ -54,6 +54,9 @@ impl QueryBuilder {
         self.order_by_kind = Some(value);
         self
     }
+    pub fn get_order_by(&self) -> Option<OrderByKind> {
+        self.order_by_kind.clone()
+    }
     pub fn limit(&mut self, value: Option<ast::Expr>) -> &mut Self {
         self.limit = value;
         self
@@ -172,6 +175,9 @@ impl SelectBuilder {
     pub fn top(&mut self, value: Option<ast::Top>) -> &mut Self {
         self.top = value;
         self
+    }
+    pub fn get_projection(&self) -> Vec<ast::SelectItem> {
+        self.projection.clone()
     }
     pub fn projection(&mut self, value: Vec<ast::SelectItem>) -> &mut Self {
         self.projection = Some(value);
@@ -297,6 +303,9 @@ impl SelectBuilder {
         self.sort_by = value;
         self
     }
+    pub fn get_sort_by(&self) -> Vec<ast::OrderByExpr> {
+        self.sort_by.clone()
+    }
     pub fn having(&mut self, value: Option<ast::Expr>) -> &mut Self {
         self.having = value;
         self
@@ -389,7 +398,9 @@ impl TableWithJoinsBuilder {
         self.relation = Some(value);
         self
     }
-
+    pub fn get_joins(&self) -> Vec<ast::Join> {
+        self.joins.clone()
+    }
     pub fn joins(&mut self, value: Vec<ast::Join>) -> &mut Self {
         self.joins = value;
         self
@@ -441,6 +452,25 @@ enum TableFactorBuilder {
 impl RelationBuilder {
     pub fn has_relation(&self) -> bool {
         self.relation.is_some()
+    }
+    pub fn get_name(&self) -> Option<String> {
+        match self.relation {
+            Some(TableFactorBuilder::Table(ref value)) => {
+                value.name.as_ref().map(|a| a.to_string())
+            }
+            _ => None,
+        }
+    }
+    pub fn get_alias(&self) -> Option<String> {
+        match self.relation {
+            Some(TableFactorBuilder::Table(ref value)) => {
+                value.alias.as_ref().map(|a| a.name.to_string())
+            }
+            Some(TableFactorBuilder::Derived(ref value)) => {
+                value.alias.as_ref().map(|a| a.name.to_string())
+            }
+            _ => None,
+        }
     }
     pub fn table(&mut self, value: TableRelationBuilder) -> &mut Self {
         self.relation = Some(TableFactorBuilder::Table(value));
