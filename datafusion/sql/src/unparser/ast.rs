a622b02053f04ce967f2fbfff8bdbd0a24261c2c
Phillip LeBlanc <phillip@leblanc.tech>
2025-04-23 01:12:29 +0900

    Support unparsing `UNION` for distinct results (#75)
    UPSTREAM NOTE: This was submitted upstream:
    https://github.com/apache/datafusion/pull/15814 and will be in DF 48

--- datafusion/sql/src/unparser/ast.rs
+++ datafusion/sql/src/unparser/ast.rs
@@ -31,6 +31,8 @@ pub struct QueryBuilder {
     fetch: Option<ast::Fetch>,
     locks: Vec<ast::LockClause>,
     for_clause: Option<ast::ForClause>,
+    // If true, we need to unparse LogicalPlan::Union as a SQL `UNION` rather than a `UNION ALL`.
+    distinct_union: bool,
 }
 
 #[allow(dead_code)]
@@ -77,6 +79,13 @@ impl QueryBuilder {
         self.for_clause = value;
         self
     }
+    pub fn distinct_union(&mut self) -> &mut Self {
+        self.distinct_union = true;
+        self
+    }
+    pub fn is_distinct_union(&self) -> bool {
+        self.distinct_union
+    }
     pub fn build(&self) -> Result<ast::Query, BuilderError> {
         let order_by = if self.order_by.is_empty() {
             None
@@ -115,6 +124,7 @@ impl QueryBuilder {
             fetch: Default::default(),
             locks: Default::default(),
             for_clause: Default::default(),
+            distinct_union: false,
         }
     }
 }
