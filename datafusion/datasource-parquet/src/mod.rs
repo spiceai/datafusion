67f09c6ed2e0b9cb1912cf45a48dcbdde84f9909
William <98815791+peasee@users.noreply.github.com>
2025-11-07 14:05:30 +1000

    fix: Make EarlyStoppingStream public (#111)
    * wip
    
    * wip
    
    * wip
    
    * wip

--- datafusion/datasource-parquet/src/mod.rs
+++ datafusion/datasource-parquet/src/mod.rs
@@ -34,6 +34,7 @@ mod writer;
 pub use access_plan::{ParquetAccessPlan, RowGroupAccess};
 pub use file_format::*;
 pub use metrics::ParquetFileMetrics;
+pub use opener::EarlyStoppingStream;
 pub use page_filter::PagePruningAccessPlanFilter;
 pub use reader::*; // Expose so downstream crates can use it
 pub use row_filter::build_row_filter;
--- datafusion/datasource-parquet/src/opener.rs
+++ datafusion/datasource-parquet/src/opener.rs
@@ -501,7 +501,7 @@ fn copy_arrow_reader_metrics(
 /// This can terminate the scan early when some dynamic filters is updated after
 /// the scan starts, so we discover after the scan starts that the file can be
 /// pruned (can't have matching rows).
-struct EarlyStoppingStream<S> {
+pub struct EarlyStoppingStream<S> {
     /// Has the stream finished processing? All subsequent polls will return
     /// None
     done: bool,
