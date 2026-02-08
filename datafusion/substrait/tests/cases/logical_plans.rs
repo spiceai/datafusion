b0c46fa277775e0634943d1ebea4d5e8d258d97c
Nuno Faria <nunofpfaria@gmail.com>
2026-02-08 12:28:58 +0000

    fix: Ensure columns are casted to the correct names with Unions (#20146)
    UPSTREAM NOTE: not required, DF original PR: from https://github.com/apache/datafusion/pull/20146
    
    ## Which issue does this PR close?
    
    - Closes #20123.
    
    ## Rationale for this change
    
    When aliasing a cast, use the original name to prevent duplicate column
    names errors.
    
    ## What changes are included in this PR?
    
    - Updated `coerce_exprs_for_schema` to use the correct name.
    - Updated tests.
    
    ## Are these changes tested?
    
    Yes.
    
    ## Are there any user-facing changes?
    
    No.
    # Conflicts:
    #	datafusion/expr/src/expr_rewriter/mod.rs
    #	datafusion/optimizer/tests/optimizer_integration.rs
    #	datafusion/substrait/tests/cases/logical_plans.rs

--- datafusion/substrait/tests/cases/logical_plans.rs
+++ datafusion/substrait/tests/cases/logical_plans.rs
@@ -217,12 +217,37 @@ mod tests {
 
         assert_snapshot!(
         plan,
-        @r#"
-            Projection: lower(sales.product) AS lower(product), sum(count(sales.product)) AS product_count
-              Aggregate: groupBy=[[sales.product]], aggr=[[sum(count(sales.product))]]
-                Aggregate: groupBy=[[sales.product]], aggr=[[count(sales.product)]]
-                  TableScan: sales
-            "#
+        @r"
+        Projection: lower(sales.product) AS lower(product), sum(count(sales.product)) AS product_count
+          Aggregate: groupBy=[[sales.product]], aggr=[[sum(count(sales.product))]]
+            Aggregate: groupBy=[[sales.product]], aggr=[[count(sales.product)]]
+              TableScan: sales
+        "
+                );
+
+        // Trigger execution to ensure plan validity
+        DataFrame::new(ctx.state(), plan).show().await?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn duplicate_name_in_union() -> Result<()> {
+        let proto_plan =
+            read_json("tests/testdata/test_plans/duplicate_name_in_union.substrait.json");
+        let ctx = add_plan_schemas_to_ctx(SessionContext::new(), &proto_plan)?;
+        let plan = from_substrait_plan(&ctx.state(), &proto_plan).await?;
+
+        assert_snapshot!(
+        plan,
+        @r"
+        Projection: foo AS col1, bar AS col2
+          Union
+            Projection: foo, bar
+              Values: (Int64(100), Int64(200))
+            Projection: x, foo
+              Values: (Int32(300), Int64(400))
+        "
                 );
 
         // Trigger execution to ensure plan validity
